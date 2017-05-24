e64cd1b340518e37558df699c22be5d4a994e90a
lihong <lihong_sail@163.com>
2017-05-24 15:17:13 +0800

    study kotlin anko

--- app/src/main/java/com/example/administrator/constrainlayoutstudy/MainActivity.kt
+++ app/src/main/java/com/example/administrator/constrainlayoutstudy/MainActivity.kt
@@ -1,16 +1,64 @@
 package com.example.administrator.constrainlayoutstudy
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
+import android.widget.EditText
+import android.widget.RelativeLayout
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
+    var rootLayout: RelativeLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+//
+//        textView.text = "Hello"
+//        textView2.text = "Kotlin"
+        rootLayout = relativeLayout {
+            val edtName = editText("gl") {
+                id = 1
+                hint = "hello"
+                textSize = 20f
+            }.lparams {
+                alignParentLeft()
+                alignParentRight()
+                alignParentTop()
+            }
 
-        textView.text = "Hello"
-        textView2.text = "Kotlin"
+            editText {
+                id = 2
+                hint = "world"
+                textSize = 20f
+            }.lparams {
+                alignParentLeft()
+                below(1)
+                leftOf(3)
+            }
+
+            button("btn1") {
+                id = 3
+                width = wrapContent
+                height = wrapContent
+                padding = 10
+                onClick {
+                    toast("${rootLayout?.find<EditText>(1)?.text}")
+                }
+            }.lparams {
+                below(1)
+                alignParentRight()
+            }
+
+            button("btn3") {
+                id = 4
+                onClick {
+                    toast(edtName.text)
+                }
+            }.lparams {
+                alignParentBottom()
+                alignParentLeft()
+                alignParentRight()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/administrator/constrainlayoutstudy/User.kt
@@ -0,0 +1,5 @@
+package com.example.administrator.constrainlayoutstudy
+
+/**
+ * Created by lihong on 2017/5/21.
+ */
